67062ad0da4bff4f9dda46211d42b65ca0bf37e1
Chris AtLee <catlee@mozilla.com>
2012-07-31 12:57:36 -0700

    set upload values

--- mozilla/staging_config.py
+++ mozilla/staging_config.py
@@ -153,5 +153,8 @@ PROJECTS = {
     },
     'dxr_mozilla-central': {
         'scripts_repo': 'http://hg.mozilla.org/users/catlee_mozilla.com/tools',
+        'upload_host': GLOBAL_VARS['stage_server'],
+        'upload_user': 'ffxbld',
+        'upload_sshkey': '/home/cltbld/.ssh/ffxbld_dsa',
     },
 }
