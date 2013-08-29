50c82fa08914981bdc969b838f61ad59ea923863
Chris AtLee <catlee@mozilla.com>
2013-08-29 11:41:01 -0400

    adding asan stuff

--- mozilla/project_branches.py
+++ mozilla/project_branches.py
@@ -204,6 +204,10 @@ PROJECT_BRANCHES = {
                 'enable_nightly': False,
                 'create_snippet': False,
             },
+            'linux64-asan': {
+                'enable_nightly': False,
+                'create_snippet': False,
+            },
             'win32': {
                 'enable_nightly': False,
                 'create_snippet': False,
